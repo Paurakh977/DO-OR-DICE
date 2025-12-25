28b86cd6b725f3ce0cd4962063105c06be40ca54
Paurakh Raj Pandey <124509859+Paurakh977@users.noreply.github.com>
2025-12-25 23:52:38 +0545

    tetsint the models and validators as an act of api game controller for now

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from __future__  import annotations
+from src import Randomizer
+from src import Player
+names= ["Jhon Doe", "Jane Smith", "Alice Johnson", "Bob Brown", "Charlie Davis"]
 def main():
-    print("Hello from do-or-dice!")
+    for player in names:
+        Player(name=player)
+    print(Player.player_arrangement) # check if arrangement is done
+
 
 
 if __name__ == "__main__":
     main()
+
